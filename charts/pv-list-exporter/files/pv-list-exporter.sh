34d31789842e1b20267d615362ab611e40f7c047
IACONO-OMNIBOOK\lucai <luca.iacono@gmail.com>
2026-06-12 20:28:24 +0000

    pv-list-exporter

---
+++ charts/pv-list-exporter/files/pv-list-exporter.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+echo "Starting extraction of PV list..."
+TARGET_FILE="{{ .Values.targetFile }}"
+kubectl get pv -o jsonpath='{.items[*].spec.csi.volumeHandle}' | tr ' ' '\n' > /data/pv-list.txt
+echo "Uploading PV list to Samba..."
+smbclient "$SAMBA_SERVER" "$PASSWORD" -U "$USERNAME" -c "put /data/pv-list.txt $TARGET_FILE; exit"
+echo "Upload completed successfully."
