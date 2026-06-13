59c5677fa58ba74764be2e3810bf1d39750deaba
IACONO-OMNIBOOK\lucai <luca.iacono@gmail.com>
2026-06-13 10:19:49 +0000

    fix script

--- charts/pv-list-exporter/files/pv-list-exporter.sh
+++ charts/pv-list-exporter/files/pv-list-exporter.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 set -e
 echo "Starting extraction of PV list..."
-TARGET_FILE="{{ .Values.targetFile }}"
+
+USERNAME=$(cat /secrets/username)
+PASSWORD=$(cat /secrets/password)
+
 kubectl get pv -o jsonpath='{.items[*].spec.csi.volumeHandle}' | tr ' ' '\n' > /data/pv-list.txt
 echo "Uploading PV list to Samba..."
 smbclient "$SAMBA_SERVER" "$PASSWORD" -U "$USERNAME" -c "put /data/pv-list.txt $TARGET_FILE; exit"
